98be298239496f00a828272cc64ca6d2c0cb753e
devbysaadan <mohdsaadan2002@gmail.com>
2024-12-01 17:48:04 -0800

    added circle class

--- part1.dart
+++ part1.dart
@@ -1,3 +1,25 @@
+abstract class Shape {
+  double calculateArea();
+  double calculatePerimeter();
+}
+
+class Circle extends Shape {
+  double radius;
+
+  Circle(this.radius);
+  @override
+  double calculateArea() {
+    return 3.14 * radius * radius;
+  }
+
+  @override
+  double calculatePerimeter() {
+    return 2 * 3.14 * radius;
+  }
+}
+
 void main() {
-  print("hello mohsin");
+  Shape circle = Circle(5.0);
+  print("Area: ${circle.calculateArea()}");
+  print("Perimeter: ${circle.calculatePerimeter()}");
 }
